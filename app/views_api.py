4fabdafb7a6f1f4b1007acf636b2e2b8ee0440c7
Alves <andrebarrela.alves@gmail.com>
2022-08-28 17:15:11 +0100

    OccurrenceObject data response added

--- app/views_api.py
+++ app/views_api.py
@@ -1164,7 +1164,7 @@ class OccurrenceObject(APIView):
         if serializer.is_valid():
             serializer.save()
 
-            return Response(status=status.HTTP_201_CREATED)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
