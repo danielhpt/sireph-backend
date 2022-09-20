a29510a35c5b9f713f73464a0c7e8108ae5f28c9
danielhpt <38636029+danielhpt@users.noreply.github.com>
2022-09-20 19:45:43 +0100

    updated tests

--- tests/views.py
+++ tests/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView, Response, status
 
+from app.models import Occurrence
 from .models import *
 
 
 class TestsList(APIView):
     swagger_schema = None
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def post(self, request):
         data = request.data.copy()
@@ -29,8 +28,7 @@ class TestsList(APIView):
 
 class TestDetail(APIView):
     swagger_schema = None
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def put(self, request, test_id):
         data = request.data.copy()
@@ -38,6 +36,21 @@ class TestDetail(APIView):
         if "action" in data:
             test_action = TestAction.objects.create(test=test, action=data["action"])
             test_action.save()
+            if data["action"] == "end test":
+                test_occurrences = TestOccurrence.objects.filter(test_id=test_id)
+                for test_occurrence in test_occurrences:
+                    occurrence = Occurrence.objects.get(pk=test_occurrence.occurrence_id)
+                    if occurrence.active:
+                        occurrence.active = False
+                        occurrence.save()
+                    team = occurrence.team
+                    if team.active:
+                        team.active = False
+                        team.save()
+                        for t in team.team_technicians.reverse():
+                            if t.active:
+                                t.active = False
+                                t.save()
         if "occurrence_id" in data:
             test_occurrence = TestOccurrence.objects.create(test=test, occurrence_id=data["occurrence_id"])
             test_occurrence.save()
